35f58edb7534b11fe01789e23d6d6d7c92c1438c
JonathanMorera <morerajonathanisfd41@gmail.com>
2023-11-01 06:54:12 -0300

    nullreferences

--- CeatN1SV/Logica/Usuario.cs
+++ CeatN1SV/Logica/Usuario.cs
@@ -18,17 +18,16 @@ namespace Logica
         {
             bool trae = false;
 
-            //OleDbDataReader reader1 = conecta.Leer("SELECT Usuario.Id, Usuario.Usuario_Nombre, Usuario.Usuario_Apellido, Permisos.Permiso_Categoria " +
-            //    "(FROM Usuario INNER JOIN Permisos ON Usuario.Usuario_Permisos = Permisos.Id) where u.Usuario_Alias = '" + usu + "' and u.Usuario_Password = '" + pass + "'");
+            OleDbDataReader reader1 = conecta.Leer("SELECT * FROM Usuario u  where u.Usuario_Alias = '" + usu + "' and u.Usuario_Password = '" + pass + "'");
             
             if (reader1.HasRows)
             {
                 while (reader1.Read())
                 {
-                    UsuarioCache.IdUsuario = reader1.GetInt32(4);
+                    UsuarioCache.IdUsuario = reader1.GetInt32(0);
                     UsuarioCache.Nombre = reader1.GetString(1);
-                    UsuarioCache.Apellido = reader1.GetString(2);
-                    UsuarioCache.Permisos = reader1.GetString(2);
+                    UsuarioCache.Apellido = reader1.GetString(1);
+                    UsuarioCache.Permisos = reader1.GetString(3);
                 
                 }
                 trae = true;
